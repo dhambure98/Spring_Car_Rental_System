84d0a0c38b32242b9b3bbd7046014afdce8b0500
dhambure98 <akila789liyanage@gmail.com>
2022-07-21 18:11:33 +0530

    Add some pakages,classes and WebAppInitializer configer

---
+++ BackEnd/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -0,0 +1,30 @@
+package lk.ijse.spring.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author : A.D.Liyanage
+ * @service : 1.0
+ **/
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "lk.ijse.spring")
+public class WebAppConfig {
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver(){
+        return new StandardServletMultipartResolver();
+    }
+
+}
