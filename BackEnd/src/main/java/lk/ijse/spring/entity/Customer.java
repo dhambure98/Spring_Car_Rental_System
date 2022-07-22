0ec1d173e9fc56c6e10b761cea6211695189eadb
dhambure98 <akila789liyanage@gmail.com>
2022-07-22 21:17:38 +0530

    Update Customer entity

--- BackEnd/src/main/java/lk/ijse/spring/entity/Customer.java
+++ BackEnd/src/main/java/lk/ijse/spring/entity/Customer.java
@@ -1,4 +1,27 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * @author : A.D.Liyanage
+ * @service : 1.0
+ **/
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Customer {
+    @Id
+    private String id;
+    private String email;
+    private String password;
+    private int contact;
+    private String license;
+    private String address;
 }
