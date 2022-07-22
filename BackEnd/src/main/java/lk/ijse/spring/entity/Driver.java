4dc1416f21ab45416f0cd2b37f33e0b5bc9a8487
dhambure98 <akila789liyanage@gmail.com>
2022-07-22 21:20:08 +0530

    Update Driver entity

--- BackEnd/src/main/java/lk/ijse/spring/entity/Driver.java
+++ BackEnd/src/main/java/lk/ijse/spring/entity/Driver.java
@@ -1,4 +1,25 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * @author : A.D.Liyanage
+ * @service : 1.0
+ **/
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Driver {
+    @Id
+    private String id;
+    private String name;
+    private String address;
+    private int contact;
 }
