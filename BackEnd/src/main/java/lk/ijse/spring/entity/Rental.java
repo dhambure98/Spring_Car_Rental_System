295d5ef9825d7711a6523245082e8b5df0be73ad
dhambure98 <akila789liyanage@gmail.com>
2022-07-22 21:11:31 +0530

    Update Rental entity

--- BackEnd/src/main/java/lk/ijse/spring/entity/Rental.java
+++ BackEnd/src/main/java/lk/ijse/spring/entity/Rental.java
@@ -1,4 +1,37 @@
 package lk.ijse.spring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ * @author : A.D.Liyanage
+ * @service : 1.0
+ **/
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Rental {
+    @Id
+    private String rentId;
+    @ManyToOne
+    @JoinColumn(name = "id",referencedColumnName = "id")
+    private Customer id;
+    @ManyToOne
+    @JoinColumn(name = "carId", referencedColumnName = "carId")
+    private Car carId;
+    @ManyToOne
+    @JoinColumn(name = "dId", referencedColumnName = "id")
+    private Driver dId;
+    private String startDate;
+    private String endDate;
+    private double price;
+    private int extraKM;
 }
