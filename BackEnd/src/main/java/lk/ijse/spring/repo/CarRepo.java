4575592b3edba6bba559f8c9f7deafd5e91a11a2
dhambure98 <akila789liyanage@gmail.com>
2022-07-26 01:35:38 +0530

    Update CarRepo

--- BackEnd/src/main/java/lk/ijse/spring/repo/CarRepo.java
+++ BackEnd/src/main/java/lk/ijse/spring/repo/CarRepo.java
@@ -1,4 +1,12 @@
 package lk.ijse.spring.repo;
 
-public interface CarRepo {
+import lk.ijse.spring.entity.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author : A.D.Liyanage
+ * @service : 1.0
+ **/
+
+public interface CarRepo extends JpaRepository<Car,String> {
 }
