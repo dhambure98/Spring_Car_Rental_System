8dcd449517522794637ffd0ba9e57f4d0abf2c4a
dhambure98 <akila789liyanage@gmail.com>
2022-07-26 01:41:17 +0530

    Update DriverRepo

--- BackEnd/src/main/java/lk/ijse/spring/repo/DriverRepo.java
+++ BackEnd/src/main/java/lk/ijse/spring/repo/DriverRepo.java
@@ -1,4 +1,12 @@
 package lk.ijse.spring.repo;
 
-public interface DriverRepo {
+import lk.ijse.spring.entity.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author : A.D.Liyanage
+ * @service : 1.0
+ **/
+
+public interface DriverRepo extends JpaRepository<Driver,String> {
 }
