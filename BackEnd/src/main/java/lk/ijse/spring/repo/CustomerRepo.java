2539cd5b37388b91ac57986b8375d583f541ae9d
dhambure98 <akila789liyanage@gmail.com>
2022-07-26 01:38:51 +0530

    Update CustomerRepo

--- BackEnd/src/main/java/lk/ijse/spring/repo/CustomerRepo.java
+++ BackEnd/src/main/java/lk/ijse/spring/repo/CustomerRepo.java
@@ -1,4 +1,15 @@
 package lk.ijse.spring.repo;
 
-public interface CustomerRepo {
+import lk.ijse.spring.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+/**
+ * @author : A.D.Liyanage
+ * @service : 1.0
+ **/
+
+public interface CustomerRepo extends JpaRepository<Customer,String> {
+    Optional<Customer> findTopByIdAndPassword(String id, String password);
 }
