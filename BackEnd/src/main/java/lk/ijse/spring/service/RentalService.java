cbf4b269510926bbb614905d998ee985cf0740c1
dhambure98 <akila789liyanage@gmail.com>
2022-07-26 17:08:37 +0530

    Update rentalService service

--- BackEnd/src/main/java/lk/ijse/spring/service/RentalService.java
+++ BackEnd/src/main/java/lk/ijse/spring/service/RentalService.java
@@ -1,4 +1,16 @@
 package lk.ijse.spring.service;
 
+import lk.ijse.spring.dto.RentalDTO;
+
+import java.util.ArrayList;
+
+/**
+ * @author : A.D.Liyanage
+ * @service : 1.0
+ **/
+
 public interface RentalService {
+    boolean addRental(RentalDTO dto);
+    boolean deleteRental(String id);
+    ArrayList<RentalDTO> getAllRentals();
 }
