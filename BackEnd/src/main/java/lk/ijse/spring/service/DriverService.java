38e8b166160d6bc38bb38492dbfcd44a8bba118d
dhambure98 <akila789liyanage@gmail.com>
2022-07-26 16:51:59 +0530

    Update DriverService service

--- BackEnd/src/main/java/lk/ijse/spring/service/DriverService.java
+++ BackEnd/src/main/java/lk/ijse/spring/service/DriverService.java
@@ -1,4 +1,19 @@
 package lk.ijse.spring.service;
 
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.dto.DriverDTO;
+
+import java.util.ArrayList;
+
+/**
+ * @author : A.D.Liyanage
+ * @service : 1.0
+ **/
+
 public interface DriverService {
+    boolean addDriver(DriverDTO dto);
+    DriverDTO searchDriver(String id);
+    boolean deleteDriver(String id);
+    boolean updateDriver(DriverDTO dto);
+    ArrayList<DriverDTO> getAllDriver();
 }
