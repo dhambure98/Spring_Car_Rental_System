9631b43bde6894057ffe93b1457993b574afdd49
dhambure98 <akila789liyanage@gmail.com>
2022-07-28 10:51:06 +0530

    Update DriverDTO

--- BackEnd/src/main/java/lk/ijse/spring/dto/DriverDTO.java
+++ BackEnd/src/main/java/lk/ijse/spring/dto/DriverDTO.java
@@ -1,4 +1,28 @@
 package lk.ijse.spring.dto;
 
-public class DriverDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author : A.D.Liyanage
+ * @service : 1.0
+ **/
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DriverDTO extends CarDTO {
+    private String id;
+    private String name;
+    private String address;
+    private int contact;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
