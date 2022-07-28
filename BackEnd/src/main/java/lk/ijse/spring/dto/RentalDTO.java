05d7e06b6362cef3f80c34f273cbcb3d22a645e8
dhambure98 <akila789liyanage@gmail.com>
2022-07-28 10:56:06 +0530

    Update RentalDTO

--- BackEnd/src/main/java/lk/ijse/spring/dto/RentalDTO.java
+++ BackEnd/src/main/java/lk/ijse/spring/dto/RentalDTO.java
@@ -1,4 +1,32 @@
 package lk.ijse.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author : A.D.Liyanage
+ * @service : 1.0
+ **/
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RentalDTO {
+    private String rentId;
+    private String startDate;
+    private String endDate;
+    private double price;
+    private int extraKM;
+    private String carId;
+    private String id;
+    private String dId;
+
+    public String getRentId() {
+        return rentId;
+    }
+
+    public void setRentId(String rentId) {
+        this.rentId = rentId;
+    }
 }
